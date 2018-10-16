370c4eb0b133f758b1f55767118a6ba0b88b14a9
JohnnyBinder <john.binder@sjhc.london.on.ca>
2018-10-16 02:12:59 -0400

    One Away

---
+++ one-away.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool OneAway(const string& s1, const string& s2) {
+	if (s1.size() - s2.size() > 1 || static_cast<int>(s1.size() - s2.size()) < -1) {
+		return false;
+	}
+
+	int i = 0; // Index for string s1
+	int j = 0; // Index for string s2
+	bool edit_found = false;
+
+	while (i != s1.size() && j != s2.size()) {
+		if (s1[i] != s2[j]) {
+			if (edit_found)
+				return false;
+			edit_found = true;
+			if (s1[i+1] == s2[j])
+				++i;
+			else if (s1[i] == s2[j+1])
+				++j;
+		}
+		++i;
+		++j;
+	}
+
+	if (!edit_found) {
+		if (i == s1.size() && j != s2.size())
+			return true;
+
+		if (i != s1.size() && j == s2.size())
+			return true;
+	}
+
+	return edit_found;
+}
+
+int main() {
+	string s1;
+	cin >> s1;
+
+	string s2;
+	cin >> s2;
+
+	if (OneAway(s1, s2))
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+}
